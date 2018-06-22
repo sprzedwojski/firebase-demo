7d6afad53e213fed3b8f4503cd94d3c5c4223215
Szymon Przedwojski <szymon.przedwojski@gmail.com>
2018-06-22 22:02:37 +0200

    Auth: added login by mail and password

--- src/sing-in-screen.js
+++ src/sing-in-screen.js
@@ -7,7 +7,8 @@ class SignInScreen extends Component {
   uiConfig = {
     signInFlow: 'popup',
     signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
     ]
   }
 
