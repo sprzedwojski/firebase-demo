900bde61e629b63aeb749f8da727b6f07d945404
Szymon Przedwojski <szymon.przedwojski@gmail.com>
2018-07-04 21:00:04 +0200

    Presentation template
    Changed API key and moved config to an ignored file.

--- src/app.js
+++ src/app.js
@@ -1,50 +1,12 @@
 import React, {Component} from 'react'
-import SignInScreen from './sing-in-screen'
+import Login from './login'
 import firebase from 'firebase'
-import Home from './home'
 
 class App extends Component {
-  state = {
-    signedIn: false,
-    elements: []
-  }
-
-  handleAuthStateChanged(signedIn) {
-    this.setState({signedIn: signedIn})
-  }
-
-  componentWillUnmount() {
-    this.unregisterAuthObserver()
-  }
-
-  componentDidMount() {
-    this.watchAuth()
-    this.watchElements()
-  }
-
-  watchElements() {
-    firebase.firestore().collection('elements').onSnapshot(querySnapshot => {
-      const elements = []
-      querySnapshot.forEach(doc => elements.push(doc.data()))
-      this.setState({elements: elements})
-    })
-  }
-
-  watchAuth() {
-    this.unregisterAuthObserver = firebase.auth()
-      .onAuthStateChanged(user => this.handleAuthStateChanged(!!user))
-  }
-
-  addElement(name) {
-    firebase.firestore().collection('elements').add({
-      name: name
-    })
-  }
-
   render() {
-    if (!this.state.signedIn)
-      return <SignInScreen />
-    return <Home logout={() => firebase.auth().signOut()} elements={this.state.elements} addElement={this.addElement}/>
+    return (
+      <div>Firebase demo, woohoo!</div>
+    )
   }
 }
 
---
+++ src/login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import firebase from 'firebase'
+
+class Login extends Component {
+  render() {
+    return (
+      <div>Login</div>
+    )
+  }
+}
+
+export default Login
