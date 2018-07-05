ed33b5039709777cf454a6f58c4f6c922aa5c4f2
Szymon Przedwojski <szymon.przedwojski@gmail.com>
2018-07-05 07:54:49 +0200

    Added cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions')
-const fetch = require('node-fetch')
+const admin = require('firebase-admin')
+admin.initializeApp()
+const translate = require('@google-cloud/translate')()
 
 exports.translateToPolish = functions.firestore
   .document('elements/{elementId}')
@@ -8,18 +10,12 @@ exports.translateToPolish = functions.firestore
     const text = snap.data().name
     console.log(`original: ${text}`)
 
-    return fetch(
-        `https://www.googleapis.com/language/translate/v2?key=${functions.config().translate.id}&source=en&target=pl&q=${text}`
-      )
-      .then(response => {
-        return response.json();
-      })
-      .then(json => {
-        let translated = json.data.translations[0].translatedText
-        console.log(translated);
-        return snap.ref.set({
-          translated: translated
-        }, {merge: true});
-      })
-      .catch(err => console.log(err));
+    return translate.translate(text,
+      {from: 'pl', to: 'en'})
+      .then(
+        (results) => {
+          return snap.ref.set({
+            translated: results[0]
+          }, {merge: true});
+        })
   })
