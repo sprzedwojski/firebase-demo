e8b6c348cc9da3acafe3d77e3f26173185689731
Szymon Przedwojski <szymon.przedwojski@polidea.com>
2018-07-09 17:03:11 +0200

    Starting the template with login <-> app screens logic

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ import Login from './login'
 import firebase from 'firebase'
 
 class App extends Component {
+  state = {
+    isLoggedIn: false
+  }
+
   render() {
+    const {isLoggedIn} = this.state
+
+    if(!isLoggedIn)
+      return <Login/>
     return (
       <div>Firebase demo, woohoo!</div>
     )
